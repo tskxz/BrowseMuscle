a2d5c666d10eb60ca9b491cc5814cc2985a6da1c
tsk <tanjilkh@gmail.com>
2022-09-06 21:35:19 +0100

    :hammer: Update statement insert

--- src/services/ExercicioService.js
+++ src/services/ExercicioService.js
@@ -1,4 +1,4 @@
-const db = require('../mysql');	
+const db = require('../mysql');
 
 module.exports = {
 	buscarTodos: () => {
@@ -22,11 +22,16 @@ module.exports = {
 			});
 		});
 	},
-	
+
 	inserir: (nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url)=> {
 		return new Promise( (aceito, rejeitado) => {
-			db.query('INSERT INTO exercicios (nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url) VALUES(?,?,?,?,?); INSERT INTO ?? (nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url) VALUES (?,?,?,?,?)', 
-				[nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url, nome_musculo, nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url],
+			db.query('INSERT INTO exercicios (nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url) VALUES(?,?,?,?,?); INSERT INTO ?? (nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url) VALUES (?,?,?,?,?); INSERT INTO ?? (nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url) VALUES (?,?,?,?,?); INSERT INTO ?? (nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url) VALUES (?,?,?,?,?);',
+				[
+					nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url,
+					nome_musculo, nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url,
+					nome_equipamento, nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url,
+					nome_dificuldade, nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url
+				],
 				(error, results)=>{
 					if(error){ rejeitado(error); return; }
 					aceito(results.insertId);
@@ -37,7 +42,7 @@ module.exports = {
 
 	alterar: (id, nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url)=> {
 		return new Promise( (aceito, rejeitado) => {
-			db.query('UPDATE exercicios SET nome_exercicio = ?, nome_equipamento = ?, nome_dificuldade = ?, nome_musculo = ?, link_url = ? WHERE id = ?', 
+			db.query('UPDATE exercicios SET nome_exercicio = ?, nome_equipamento = ?, nome_dificuldade = ?, nome_musculo = ?, link_url = ? WHERE id = ?',
 				[nome_exercicio, nome_equipamento, nome_dificuldade, nome_musculo, link_url, id],
 				(error, results)=>{
 					if(error){ rejeitado(error); return; }
