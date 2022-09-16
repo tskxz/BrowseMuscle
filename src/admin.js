efbccb424cf98b3fee5d556f27fe065e8ac275d8
tsk <tanjilkh@gmail.com>
2022-09-16 19:22:57 +0100

    :bulb: Comentários e remover uma rota

--- src/admin.js
+++ src/admin.js
@@ -2,15 +2,20 @@ const express = require('express');
 const admin = express.Router();
 const path = require('path');
 
+// Rotas para o admin
 
+/*
+admin/post_exercicios
+admin/view
+admin/
+*/
+
+// Inserir exercícios
 admin.get('/post_exercicios', function(req, res) {
 	res.sendFile(path.join(__dirname, '/views/admin/exercicios_POST.html'))
 })
 
-admin.get('/view', function(req, res) {
-	res.sendFile(path.join(__dirname, '/views/admin/exercicios.html'))
-})
-
+// Tabela de todos os exercícios 
 admin.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '/views/admin/tabela_exercicios.html'))
 })
