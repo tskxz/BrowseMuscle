d36f10e8c578ed91e25039d8669bf4c223bc73a9
tsk <tanjilkh@gmail.com>
2022-09-16 21:49:03 +0100

    :bulb: Comentários

--- src/admin.js
+++ src/admin.js
@@ -12,12 +12,12 @@ admin/
 
 // Inserir exercícios
 admin.get('/post_exercicios', function(req, res) {
-	res.sendFile(path.join(__dirname, '/views/admin/exercicios_POST.html'))
+	res.sendFile(path.join(__dirname, '/views/admin/exercicios_POST.html')) // Inserir exercícios através do form HTML
 })
 
 // Tabela de todos os exercícios 
 admin.get('/', function(req, res){
-	res.sendFile(path.join(__dirname, '/views/admin/tabela_exercicios.html'))
+	res.sendFile(path.join(__dirname, '/views/admin/tabela_exercicios.html')) // Visualizar os exercícios através da tabela em HTML
 })
 
 module.exports = admin;
