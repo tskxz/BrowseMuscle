2ef8fd310176787714c28f596be374d6b1d982d7
tsk <tanjilkh@gmail.com>
2022-09-16 20:22:01 +0100

    :bulb: Comentários

--- src/services/MusculoService.js
+++ src/services/MusculoService.js
@@ -1,6 +1,8 @@
 const db = require('../mysql');
 
 module.exports = {
+
+	// Funcao para visualizar os musculos
 	visualizarTodos: () => {
 		return new Promise( (aceito, reijeitado) =>{
 			db.query('SELECT * FROM musculo', (error, results) => {
