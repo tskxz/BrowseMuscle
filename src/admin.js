96c0c9277c36c17fa47eba8d85b7651f21354a60
tsk <tanjilkh@gmail.com>
2022-09-22 20:43:47 +0100

    :sparkles: PUT form data

--- src/admin.js
+++ src/admin.js
@@ -15,7 +15,11 @@ admin.get('/post_exercicios', function(req, res) {
 	res.sendFile(path.join(__dirname, '/views/admin/exercicios_POST.html')) // Inserir exercícios através do form HTML
 })
 
-// Tabela de todos os exercícios 
+admin.get('/update_exercicios', function(req, res) {
+	res.sendFile(path.join(__dirname, '/views/admin/exercicios_PUT.html')) // Atualizar exercícios através do form HTML
+})
+
+// Tabela de todos os exercícios
 admin.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '/views/admin/tabela_exercicios.html')) // Visualizar os exercícios através da tabela em HTML
 })
