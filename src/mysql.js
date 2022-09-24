1b5987a1515617a7ccb40a12c202167123c18073
tsk <tanjilkh@gmail.com>
2022-09-24 18:58:02 +0100

    :zap: Valores no input

--- src/mysql.js
+++ src/mysql.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
 	host: process.env.DB_HOST,				// localhost
 	user: process.env.DB_USER,				// root
-	password: process.env.DB_PASS_LINUX,	// root ou palavra passe vazia
+	password: process.env.DB_PASS,			// root ou palavra passe vazia
 	database: process.env.DB_NAME,			// BrowseMuscle
 	multipleStatements: true 				// Permitir mais de que uma query 
 });
