a10f4b56cb24022bde65e2e826cb9ecca226b863
Tanjil <tanjilkh@gmail.com>
2022-10-05 16:18:02 +0100

    :alien: API Dificuldades

---
+++ src/controllers/Dificuldades/InicianteController.js
@@ -0,0 +1,24 @@
+const InicianteService = require('../../services/Dificuldades/InicianteService');
+
+module.exports = {
+
+	visualizarTodos: async(req, res) => {
+		let json = {error: '', result:[]};
+
+		let iniciante = await InicianteService.visualizarTodos();
+
+		for(let i in iniciante){
+			json.result.push({
+				id: iniciante[i].id,
+				nome_exercicio: iniciante[i].nome_exercicio,
+                nome_equipamento: iniciante[i].nome_equipamento,
+                nome_dificuldade: iniciante[i].nome_dificuldade,
+                nome_musculo: iniciante[i].nome_musculo
+			});
+		}
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-  Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+		res.json(json);
+	}
+}
