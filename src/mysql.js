a2bdbc72750cd80173fe1b5fb4c1a0e5dd9162e9
tsk <tanjilkh@gmail.com>
2022-10-07 08:39:02 +0100

    :zap: pull master & vps to host

--- src/mysql.js
+++ src/mysql.js
@@ -1,20 +1,11 @@
 const mysql = require('mysql');
 
 // Conectar a base de dados
-var osValue = process.platform;
-
-if(osValue== 'linux') {
-	console.log('Sistema Operativo: ' + osValue);
-    DB_PASS = "root"
-} else {
-	console.log('Sistema Operativo: ' + osValue);
-	DB_PASS = "";
-}
 
 const connection = mysql.createConnection({
 	host: process.env.DB_HOST,				// 4acd-132-145-18-76.eu.ngrok.io
 	user: process.env.DB_USER,				// root
-	password: DB_PASS,						// root ou palavra passe vazia
+	password: process.env.DB_PASS,						// root ou palavra passe vazia
 	database: process.env.DB_NAME,			// BrowseMuscle
 	multipleStatements: true,				// Permitir mais de que uma query
 	connectTimeout: 3000000
