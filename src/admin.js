e29afc0ce217373b227f61674ab9012006d0d147
tsk <tanjilkh@gmail.com>
2022-10-07 08:42:29 +0100

    :open_file_folder: Mudar rotas e mover ficheiros

--- src/admin.js
+++ src/admin.js
@@ -12,16 +12,16 @@ admin/
 
 // Inserir exercícios
 admin.get('/post_exercicios', function(req, res) {
-	res.sendFile(path.join(__dirname, '/views/admin/exercicios_POST.html')) // Inserir exercícios através do form HTML
+	res.sendFile(path.join(__dirname, '/views/admin/exercicios/exercicios_POST.html')) // Inserir exercícios através do form HTML
 })
 
 admin.get('/update_exercicios', function(req, res) {
-	res.sendFile(path.join(__dirname, '/views/admin/exercicios_PUT.html')) // Atualizar exercícios através do form HTML
+	res.sendFile(path.join(__dirname, '/views/admin/exercicios/exercicios_PUT.html')) // Atualizar exercícios através do form HTML
 })
 
 // Tabela de todos os exercícios
 admin.get('/', function(req, res){
-	res.sendFile(path.join(__dirname, '/views/admin/tabela_exercicios.html')) // Visualizar os exercícios através da tabela em HTML
+	res.sendFile(path.join(__dirname, '/views/admin/exercicios/tabela_exercicios.html')) // Visualizar os exercícios através da tabela em HTML
 })
 
 module.exports = admin;
