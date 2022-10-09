6d99741636d562fd1f9acf856a7c679675495657
tsk <tanjilkh@gmail.com>
2022-10-09 10:43:28 +0100

    :zap: host to vps

--- src/views/admin/exercicios/post_script.js
+++ src/views/admin/exercicios/post_script.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/api/exercicio/', {
+fetch('https://ec53-132-145-18-76.eu.ngrok.io/api/exercicio/', {
 	method: 'POST',
 	headers: {
 			'Content-Type': 'application/json'
