de3a612510a7a11cb4ab61cb15be50a11e058d09
tsk <tanjilkh@gmail.com>
2022-10-14 08:52:59 +0100

    :bug: Corrigir erro misstype foreign key

--- db/browsemuscle.sql
+++ db/browsemuscle.sql
@@ -1,42 +1,42 @@
-/*  Base de dados BrowseMuscle */
+    /*  Base de dados BrowseMuscle */
 
--- Criar base de dados
+    -- Criar base de dados
 
-create database BrowseMuscle;
-use BrowseMuscle;
+    create database BrowseMuscle;
+    use BrowseMuscle;
 
--- Estrutura da tabela musculos
+    -- Estrutura da tabela musculos
 
-create table `musculos` (
-    `id` int(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nome` varchar(100) NOT NULL
-);
+    create table `musculos` (
+        `id` int(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `nome` varchar(100) NOT NULL
+    );
 
--- Estrutura da tabela equipamentos
+    -- Estrutura da tabela equipamentos
 
-create table `equipamentos` (
-    `id` int(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nome` varchar(100) NOT NULL
-);
+    create table `equipamentos` (
+        `id` int(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `nome` varchar(100) NOT NULL
+    );
 
--- Estrutura da tabela dificuldades
+    -- Estrutura da tabela dificuldades
 
-create table `dificuldades` (
-    `id` int(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nome` varchar(100) NOT NULL
-);
+    create table `dificuldades` (
+        `id` int(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `nome` varchar(100) NOT NULL
+    );
 
--- Estrutura da tabela exercícios
+    -- Estrutura da tabela exercícios
 
-create table `exercícios` (
-    `id` int(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nome` varchar(100) NOT NULL,
-    `equipamento_id` int(6) NOT NULL,
-    `dificuldade_id` int(6) NOT NULL,
-    `musculo_id` int(6) NOT NULL,
+    create table `exercícios` (
+        `id` int(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `nome` varchar(100) NOT NULL,
+        `equipamento_id` int(6) NOT NULL,
+        `dificuldade_id` int(6) NOT NULL,
+        `musculo_id` int(6) NOT NULL,
 
-    FOREIGN KEY (equipamento_id) REFERENCES equipamentos(id),
-    FOREIGN KEY (dificuldade_id) REFERENCES dificuldade_id(id),
-    FOREIGN KEY (musculos_id) REFERENCES musculos(id)
-);
+        FOREIGN KEY (equipamento_id) REFERENCES equipamentos(id),
+        FOREIGN KEY (dificuldade_id) REFERENCES dificuldade_id(id),
+        FOREIGN KEY (musculo_id) REFERENCES musculos(id)
+    );
 
