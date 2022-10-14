eb5abd3999da76e2612016ddbbccc2ec0985b929
tsk <tanjilkh@gmail.com>
2022-10-14 08:42:28 +0100

    :hammer: sql db foreign key

--- db/BrowseMuscle_13_10_2022.sql
+++ db/BrowseMuscle_13_10_2022.sql
@@ -21,7 +21,8 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-
+create database BrowseMuscle;
+use BrowseMuscle;
 --
 -- Table structure for table `abdominais`
 --
