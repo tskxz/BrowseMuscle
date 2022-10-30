1130080a65be006798ade5eedba85627fe49d2eb
Tanjil <tanjilkh@gmail.com>
2022-10-30 12:58:10 +0000

    :zap: Buscar os ids de cada tabela

--- src/controllers/ExercicioController.js
+++ src/controllers/ExercicioController.js
@@ -14,8 +14,11 @@ module.exports = {
 				id: exercicios[i].id,
 				exercicio: exercicios[i].exercicio,
 				equipamento: exercicios[i].equipamento,
+				equipamento_id: exercicios[i].equipamento_id,
 				dificuldade: exercicios[i].dificuldade,
-				musculo: exercicios[i].musculo
+				dificuldade_id: exercicios[i].dificuldade_id,
+				musculo: exercicios[i].musculo,
+				musculo_id: exercicios[i].musculo_id
 			});
 		}
 		res.setHeader('Access-Control-Allow-Origin', '*');
