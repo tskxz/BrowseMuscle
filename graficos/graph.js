2c263b016285a6800ba08369cbf80b6ed701a305
tsk <tanjilkh@gmail.com>
2022-11-03 14:05:39 +0000

    :rocket: Deploy stuff

--- graficos/graph.js
+++ graficos/graph.js
@@ -5,7 +5,7 @@ const data = {
 	datasets: [
 	{
 		label: 'Dataset 1',
-		data: [12, 29, 12], // Proteina: 12 | Hidratos de carbono: 29 | Gordura: 12
+		data: [7, 58, 25], // Proteina: 12 | Hidratos de carbono: 29 | Gordura: 12
 		backgroundColor: [
 		'rgba(255, 99, 132)',
 		'rgba(54, 162, 235)',
@@ -25,7 +25,7 @@ const lineChart = new Chart(ctx, {
     		},
     		title: {
     			display: true,
-    			text: 'Macronutrientes da pizza hut carbonara - 100g'
+    			text: 'Macronutrientes Bolachas Cookies Pingo Doce - 125g'
     		}
     	}
     }
