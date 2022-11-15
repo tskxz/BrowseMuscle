8bc7c30e3cbfab782ad0ec1f78e7db6c45344a05
tsk <tanjilkh@gmail.com>
2022-11-15 12:26:08 +0000

    :rocket: Base de dados - Tabela utilizadores

--- db/utilizadores/utilizadores.sql
+++ db/utilizadores/utilizadores.sql
@@ -1,15 +1,68 @@
--- Database: `browsemuscle`
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5ubuntu0.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 15, 2022 at 01:13 PM
+-- Server version: 5.7.40-0ubuntu0.18.04.1
+-- PHP Version: 7.2.34-28+ubuntu18.04.1+deb.sury.org+1
 
-use browsemuscle;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
--- Estrutura da tabela utilizador
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `BrowseMuscle`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `utilizadores`
+--
 
 CREATE TABLE `utilizadores` (
-  `id` int(6) NOT NULL,
-  `nome_completo` varchar(100) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `país` varchar(100) NOT NULL,
-  `cidade` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `primeiro_nome` varchar(25) NOT NULL,
+  `ultimo_nome` varchar(25) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `num_telemovel` varchar(25) NOT NULL,
   `password` varchar(100) NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `utilizadores`
+--
+
+INSERT INTO `utilizadores` (`id`, `username`, `primeiro_nome`, `ultimo_nome`, `email`, `num_telemovel`, `password`) VALUES
+(1, 'tsk', 'Tanjil', 'Khan', 'tanjlkh@gmail.com', '+351914721543', '$2b$10$mkweRrVdhpX.zAApdPYhju09ljCBjIQHXf2FBcqwpw3v6NBtva48K'),
+(2, 'guedes', 'Joao', 'Guedes', 'joaoguedes@gmail.com', '+3519339102300', '$2b$10$F92lDO90lwDUUFaYtikQZuQFSBWs.LwdcTTEU0up.3PTtUgstaD9a');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `utilizadores`
+--
+ALTER TABLE `utilizadores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `utilizadores`
+--
+ALTER TABLE `utilizadores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
