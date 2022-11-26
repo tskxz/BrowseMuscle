0c0caa78b7d376d01e8f932ccc0049df4c27f52c
tsk <tanjilkh@gmail.com>
2022-11-26 10:03:06 +0000

    :art: Pagina editar perfil

--- src/routes/app.js
+++ src/routes/app.js
@@ -36,6 +36,15 @@ app.get('/meu_perfil', checkAuthenticated, function(req, res) {
 	})
 })
 
+app.get('/meu_perfil/editar', checkAuthenticated, function(req, res){
+	res.render('app/utilizador/editar_perfil.hbs', { 
+		user: req.user,
+		username: req.user.username,
+		primeiro_nome: req.user.primeiro_nome,
+		ultimo_nome: req.user.ultimo_nome,
+		email: req.user.email
+	})
+})
 app.get('/perfil/:username', checkAuthenticated, UtilizadorController.perfil)
 
 function checkAuthenticated(req, res, next){
