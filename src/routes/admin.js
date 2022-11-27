af1ce513e46b419a10d2b076009c4c2466d5a0e3
tsk <tanjilkh@gmail.com>
2022-11-27 10:03:39 +0000

    :passport_control: Permissão nas rotas administrador

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -9,20 +9,20 @@ const AlimentoController = require('../controllers/Alimentos/AlimentoController'
 const ExercicioController = require('../controllers/ExercicioController')
 
 // Rotas para administração de exercícios
-admin.get('/main_exercicios', ExercicioController.main) 			// Visualizar tabela exercícios
-admin.get('/editar_exercicio/:id', ExercicioController.editar)		// Página para editar exercício
-admin.post('/editar_exercicio/:id', ExercicioController.atualizar)	// Atualizar o exercício
-admin.get('/adicionar_exercicio', ExercicioController.adicionar_form)	// Página para adicionar exercício
-admin.post('/adicionar_exercicio', ExercicioController.adicionar)	// Adicionar exercício
-admin.get('/apagar_exercicio/:id', ExercicioController.eliminar)	// Apagar exercício
+admin.get('/main_exercicios', authUser, authRole(2), ExercicioController.main) 			// Visualizar tabela exercícios
+admin.get('/editar_exercicio/:id', authUser, authRole(2), ExercicioController.editar)		// Página para editar exercício
+admin.post('/editar_exercicio/:id', authUser, authRole(2), ExercicioController.atualizar)	// Atualizar o exercício
+admin.get('/adicionar_exercicio', authUser, authRole(2), ExercicioController.adicionar_form)	// Página para adicionar exercício
+admin.post('/adicionar_exercicio', authUser, authRole(2), ExercicioController.adicionar)	// Adicionar exercício
+admin.get('/apagar_exercicio/:id', authUser, authRole(2), ExercicioController.eliminar)	// Apagar exercício
 
 // Rotas para administração de alimentos
-admin.get('/main_alimentos', AlimentoController.main)				// Visualizar tabela alimentos
-admin.get('/adicionar_alimento', AlimentoController.adicionar)		// Página para adicionar alimento
-admin.post('/adicionar_alimento', AlimentoController.adicionar)		// Adicionar alimento
-admin.get('/apagar_alimento/:id', AlimentoController.eliminar)		// Apagar alimento
-admin.get('/editar_alimento/:id', AlimentoController.atualizar)		// Página para editar alimento
-admin.post('/editar_alimento/:id', AlimentoController.atualizar)	// Atualizar o alimento
+admin.get('/main_alimentos', authUser, authRole(2), AlimentoController.main)				// Visualizar tabela alimentos
+admin.get('/adicionar_alimento', authUser, authRole(2), AlimentoController.adicionar)		// Página para adicionar alimento
+admin.post('/adicionar_alimento', authUser, authRole(2), AlimentoController.adicionar)		// Adicionar alimento
+admin.get('/apagar_alimento/:id', authUser, authRole(2), AlimentoController.eliminar)		// Apagar alimento
+admin.get('/editar_alimento/:id', authUser, authRole(2), AlimentoController.atualizar)		// Página para editar alimento
+admin.post('/editar_alimento/:id', authUser, authRole(2), AlimentoController.atualizar)	// Atualizar o alimento
 
 // Rota para administração
 admin.get('/dashboard', authUser, authRole(2), function(req, res){
