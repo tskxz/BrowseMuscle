1fa6c6d87ae3d7e7cfbb3b13e0b3892c28d1c667
tsk <tanjilkh@gmail.com>
2022-12-14 13:59:29 +0000

    :zap: Utilizador - admin

---
+++ seeders/20221214135701-utilizador-admin.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Utilizadores', [
+    {
+      username: 'dev',
+      primeiro_nome: 'dev',
+      ultimo_nome: 'dev',
+      email: 'dev',
+      num_telemovel: '',
+      password: 'dev',
+      id_cargo: '2',
+      treinos_concluidos: '0',
+      foto: '',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+   ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
