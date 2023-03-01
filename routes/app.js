5710efc8af047aab5fd78f0a69263eeaf39e4598
tsk <tanjilkh@gmail.com>
2023-03-01 14:54:30 +0000

    :art: Botao responsive

--- routes/app.js
+++ routes/app.js
@@ -78,7 +78,7 @@ app.get('/meu_perfil', checkAuthenticated, function (req, res) {
 		descricao: req.user.descricao,
 		id_cargo: req.user.id_cargo,
 		treinos_concluidos: req.user.treinos_concluidos,
-		createdAt: req.user.createdAt,
+		createdAt: req.user.createdAt.toLocaleDateString('pt-PT', { year: 'numeric', month: '2-digit', day: '2-digit' }),
 		foto: req.user.foto
 	})
 })
